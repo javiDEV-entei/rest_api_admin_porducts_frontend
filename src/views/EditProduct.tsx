7029c9393e02d1b36b85780570696eac8f4c57b0
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-08-09 11:37:32 -0300

    imports no utilizados eliminados
    se elimino el import de addProduct que no estaba siendo usado en el archivo editProduct del repo del frontend

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -1,6 +1,6 @@
 import { Link, Form, useActionData, type ActionFunctionArgs, redirect, type LoaderFunctionArgs, useLoaderData} from "react-router-dom"
 import ErrorMessage from "../components/ErrorMessage"
-import { addProduct, getProductById, updateProduct } from "../services/ProductService"
+import {  getProductById, updateProduct } from "../services/ProductService"
 import type { Product } from "../types"
 import ProductForm from "../components/ProductForm"
 
